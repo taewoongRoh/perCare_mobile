0aa8c53849c8e55afec39865cd294c21cd200a72
taewoongRoh <ooo_ooo_@nate.com>
2016-08-07 19:48:10 +0900

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -20,3 +20,53 @@ appomat.app = {
     }
 
 };
+
+
+
+
+$(document).on("pageshow", "#maps", function() {
+
+
+        var container = document.getElementById('map');
+        var options = {
+            center: new daum.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        };
+
+        var map = new daum.maps.Map(container, options);
+
+        map.relayout();
+    });
+
+
+    $(document).on("pageshow", "#select_animal", function(event) {
+        alert("pageshow event fired - select_animal page is now shown");
+    });
+
+
+
+    $(function() {
+
+        $(".menubar").click(function() {
+            if ($("#right-panel").hasClass('menu-open')) {
+                $("#right-panel").removeClass('menu-open');
+                $('.content').removeClass('background-active');
+                $('.animal_list').removeClass('unclickable');
+
+            } else {
+                $("#right-panel").addClass('menu-open');
+                $('.content').addClass('background-active');
+                $('.animal_list').addClass('unclickable');
+            }
+        })
+
+
+        $(".main_content").click(function() {
+            if ($("#right-panel").hasClass('menu-open')) {
+                $("#right-panel").removeClass('menu-open');
+                $('.content').removeClass('background-active');
+                $('.animal_list').removeClass('unclickable');
+            }
+
+        });
+    });
